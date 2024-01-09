a45bdeb5283d819703bd82fdf04ab7d6a148f7af
Anzaruu <isip_a.s.zarubina@mpt.ru>
2024-01-09 15:26:20 +0300

    tired

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
 import initSlider from "./modules/_slider.js"
 import requestDialogInit from "./modules/_request-dialog.js"
+import menuInit from "./modules/_menu.js"
 
 const init = () => {
     initSlider()
     requestDialogInit()
+    menuInit()
 }
 
 init()
--- src/js/modules/_slider.js
+++ src/js/modules/_slider.js
@@ -4,7 +4,17 @@ import { Navigation, Pagination } from 'swiper/modules';
 const initSlider = () => {
     const swiper = new Swiper('.reviews__slider', {
         modules: [Navigation, Pagination],
-        slidesPerView: 3,
+        breakpoints:{
+            370: {
+              slidesPerView: 1,
+            },
+            710: {
+              slidesPerView: 2,
+            },
+            1200: {
+              slidesPerView: 3,
+            },
+          },
         spaceBetween: 20,
         navigation: {
             prevEl: '.slider__prev',
